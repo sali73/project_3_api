389be6df583483812d78b418fcce414b4a1d5097
sali mohamed <sally.elgendy@hotmail.com>
2020-06-08 18:48:42 -0400

    add bootstrap link to frontend

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-// dependencies
+//////////////////
+// Dependencies
+/////////////////
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const productsController = require('./controllers/routes.js')
 const cors = require('cors');
 
-// global variables
+////////////////////
+// Global Variables
+///////////////////
 const {
     PORT = 3001,
     MONGO_URI = 'mongodb://localhost:27017/products',
 } = process.env;
-
 const db = mongoose.connection;
 
+////////////////////
 // db connection
+///////////////////
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -21,11 +26,11 @@ mongoose.connect(MONGO_URI, {
 });
 db.once('open', () => console.log('connected to mongo at', MONGO_URI));
 db.on('error', (err) => console.log('🚨🚨🚨', err));
+
 /////////////////////
 // CORS
 ////////////////////
 const whitelist = ['http://localhost:3000'];
-
 const corsOptions = {
    origin: function (origin, callback) {
        if (whitelist.indexOf(origin) !== -1) {
@@ -35,15 +40,23 @@ const corsOptions = {
        }
    },
 };
-// middleware
+
+/////////////////
+// Middleware
+////////////////
 app.use(cors());
 app.use(express.json());
 app.use('/products', productsController);
 
+//////////////////////////////////////////////////////////////////////////
 // test ============================🚧🚧🚧 DELETE BEFORE SUBMIT 🚧🚧🚧
 // app.get('/', (req, res) => {
 //     res.send('Hello World');
 // })
+/////////////////////////////////////////////////////////////////////////
 
+
+//////////////
 // listen
+/////////////
 app.listen(PORT, () => console.log('listening on', PORT));
