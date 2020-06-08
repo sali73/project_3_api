cecaebb94911b1ad5f5de7f4ac7b5fe63d1a749a
christian <ceunger91@gmail.com>
2020-06-08 13:23:11 -0400

    server.js is set up

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 // dependencies
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-// Port 3001 - React will live on 3000
-const PORT = process.env.PORT || 3001
+// globals
+const {
+    PORT = 3001,
+    MONGO_URI = 'mongodv://localhost:27017' // fill in db name 
+} = process.env;
+const db = mongoose.connection;
+
+// db connection
+// mongoose.connect(MONGO_URI, {
+//     useNewUrlParser: true,
+//     useFindAndModify: true,
+//     useUnifiedTopology: true,
+// });
+// db.once('open', () => console.log('connected to mongo at', MONGO_URI));
+// db.on('error', (err) => console.log('🚨 mongodb error:', err));
 
 // middleware
+app.use(express.json());
+// app.use('/products', products);
 
-
-// test
+// test ========== DELETE before submit
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
