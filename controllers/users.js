8f7ad729b1e851bffd7012582a93c442191273b7
christian <ceunger91@gmail.com>
2020-06-10 13:06:20 -0400

    added password requirement checks - we can adjust these as needed/comment out for testing

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,39 @@
+// dependencies
 const express = require('express');
 const users = express.Router();
 const bcrypt = require('bcrypt');
-
 const User = require('../models/users.js');
 
 // New User Authentication
 users.post('/', (req, res) => {
+    // destructure username and password out of request
     const { username, password } = req.body;
+    // make sure both fields were entered
     if (!username || !password) {
         return res.status(400).json({ message: 'Please enter a Username and Password' });
     }
+    // check for password length requirement
+    if (password.length < 6) {
+        return res.status(400).json({ message: 'Password must be at least 8 characters' });
+    }
+    // Check passwprd for at least one of each:
+    // - Uppercase Letter
+    // - Lowercase Letter
+    // - number
+    // - special character
+    if (!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/g)) {
+        return res.status(400).json({ message: 'Password does not meet the requirements' })
+    }
+    // check if username already exists
     User.findOne({ username })
         .then(user => {
             if (user) return res.status(400).json({ message: 'Username already exists' });
-
+            // if not, create new User object
             const newUser = new User({
                 username,
                 password,
             });
+            // encrypt new user password
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newUser.password, salt, (err, hash) => {
                     if(err) throw err;
@@ -33,6 +49,7 @@ users.post('/', (req, res) => {
                         })
                 })
             } )
+            // add user to the database
             User.create(newUser)
         })
 });
