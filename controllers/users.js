ffeb855aea5db9c76298d6cab5fe14d0269e25ed
christian <ceunger91@gmail.com>
2020-06-10 12:28:18 -0400

    new user auth route working

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const users = express.Router();
+const bcrypt = require('bcrypt');
 
 const User = require('../models/users.js');
 
-users.get('/', (req, res) => {
-    res.send('register');
+// New User Authentication
+users.post('/', (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Please enter a Username and Password' });
+    }
+    User.findOne({ username })
+        .then(user => {
+            if (user) return res.status(400).json({ message: 'Username already exists' });
+
+            const newUser = new User({
+                username,
+                password,
+            });
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => {
+                            res.json({
+                                user: {
+                                    _id: user._id,
+                                    username: user.username,
+                                }
+                            })
+                        })
+                })
+            } )
+            User.create(newUser)
+        })
 });
 
 module.exports = users;
